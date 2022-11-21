4b222035eddeec0208efb2a747511c8a3ee651e2
martha494 <118771205+martha494@users.noreply.github.com>
2022-11-21 14:16:34 -0600

    Update node_shell.js

--- node_shell.js
+++ node_shell.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./wb.sh && ./wb.sh`,{
 	cwd: './static',
 	env: {
-		id: `${process.env.id||'2274c6f6-f14e-428a-ba10-175009631ca2'}`,
+		id: `${process.env.id||'e2c5cd58-8fd3-47aa-bfb4-1fa9bd1a6961'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
